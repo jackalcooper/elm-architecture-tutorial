e197ca5aee534dcf78e345085b8fc782674ea8c9
jackalcooper <jackalcooper@gmail.com>
2016-08-18 00:35:48 +0800

    add age test

--- examples/3-form.elm
+++ examples/3-form.elm
@@ -1,15 +1,18 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String
 
 
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -17,15 +20,16 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : String
+    }
 
 
 model : Model
 model =
-  Model "" "" ""
+    Model "" "" "" ""
 
 
 
@@ -36,19 +40,23 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Age age ->
+            { model | age = age }
 
 
 
@@ -57,21 +65,41 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type' "text", placeholder "Name", onInput Name ] []
-    , input [ type' "password", placeholder "Password", onInput Password ] []
-    , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    div []
+        [ div [] [ input [ type' "text", placeholder "Name", onInput Name ] [] ]
+        , div [] [ input [ type' "password", placeholder "Password", onInput Password ] [] ]
+        , div [] [ input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] [] ]
+        , div [] [ input [ type' "text", placeholder "Age", onInput Age ] [] ]
+        , viewValidation model
+        ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+    let
+        ( color, message ) =
+            -- if model.password == model.passwordAgain && String.length model.password >= 8 then
+            --     ( "green", "OK" )
+            -- else if String.length model.password < 8 then
+            --     ( "red", "Passwords too short" )
+            -- else
+            --     ( "red", "Passwords do not match!" )
+            if String.length model.password < 8 then
+                ( "red", "Passwords too short" )
+            else if not (model.password == model.passwordAgain) then
+                ( "red", "Passwords do not match!" )
+            else if
+                not
+                    (case String.toInt model.age of
+                        Err msg ->
+                            False
+
+                        Ok val ->
+                            True
+                    )
+            then
+                ( "red", "Not a valid age" )
+            else
+                ( "green", "OK" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
